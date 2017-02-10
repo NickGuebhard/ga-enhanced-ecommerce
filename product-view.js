042aa5dc8b0a83ebbde651940487d5c11ce3dfd1
NickGuebhard <nick@datatribe.co.nz>
2017-02-10 15:56:04 +1300

    Create product-view.js

---
+++ product-view.js
@@ -0,0 +1,16 @@
+
+dataLayer.push({
+  'ecommerce': {
+    'detail': {
+      'actionField': {'list': 'Apparel Gallery'},    // 'detail' actions have an optional list property.
+      'products': [{
+        'name': 'Triblend Android T-Shirt',         // Name or ID is required.
+        'id': '12345',
+        'price': '15.25',
+        'brand': 'Google',
+        'category': 'Apparel',
+        'variant': 'Gray'
+       }]
+     }
+   }
+});
