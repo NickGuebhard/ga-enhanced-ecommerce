f646067cc258e2fa5efdf053573c0bf625bf1eb0
NickGuebhard <nick@datatribe.co.nz>
2017-02-13 13:52:56 +1300

    Create product-checkout-option.js

---
+++ product-checkout-option.js
@@ -0,0 +1,13 @@
+/**
+ * A function to handle a click leading to a checkout option selection.
+ */
+function onCheckoutOption(step, checkoutOption) {
+  dataLayer.push({
+    'event': 'checkoutOption',
+    'ecommerce': {
+      'checkout_option': {
+        'actionField': {'step': step, 'option': checkoutOption}
+      }
+    }
+  });
+}
